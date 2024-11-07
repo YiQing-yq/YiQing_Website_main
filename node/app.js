79c7322ab56996eb41ddcf437f23b38e8c0086de
YiQing-yq <onesevening@gmail.com>
2024-11-07 12:53:01 +0800

    Squashed commit of the following:
    commit a892c3d8b5b10c5d11cf92c9d78fd3a3ce78c440
    Author: YiQing-yq <onesevening@gmail.com>
    Date:   Thu Nov 7 12:44:32 2024 +0800
    
        Put The aphla Version

---
+++ node/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var path = require('path');
+require('./js/connectdb.js');
+const app = express();
+const port = 3000;
+ 
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/HTML/index.html');
+});
+ 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
